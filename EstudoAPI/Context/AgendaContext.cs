42c219091a6c44d8640f565dfc29238f26834353
Ju Couto <jnalmeida@ymail.com>
2022-09-26 14:01:01 -0300

    API Agenda

---
+++ EstudoAPI/Context/AgendaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using teste.Entities;
+
+namespace teste.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
